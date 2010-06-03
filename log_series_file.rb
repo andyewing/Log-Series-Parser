f7fa2700e8b3b59519d9657db39023571e18d0b7
unknown <maewing@.cisco.com>
2010-06-03 13:59:38 -0400

    moved set/subsection/none logic into a separate method.

--- log_series_file.rb
+++ log_series_file.rb
@@ -36,25 +36,29 @@ require 'csv'
 #Parses a series of the same logs with sub-sections into a csv
 class LogSeriesFile < File
 
-	DEBUG = true
-
-	#some constants: Section divider
+	#some constants: Section divider and debug flag
 	SECTION_DIVIDER = "---"
-
+	DEBUG = true
+	
+	# Instance Variables
+	attr_accessor :outfile, :data_arr, :header_arr, :gathering_state
+	
 	#parse takes in entryregex, for identifyint the start of new log entries,
 	#subregex, for any sub-sections of similar kind in each log entry
 	#and parseregex, an array of all regex's of note
 	def log_series_parse(*log_regexes)
 		
-		
-		#First we initialize a temporary array
-		temparr = []
-		
 		#And we open the output file
 		#!! Throw--Catch this...
+		@outfile = CSV.open("#{self.path}.csv",'wb')
 		
-		parseout = CSV.open("#{self.to_path}.csv",'wb')
-		#parseout = CSV.open("parseout.csv",'wb')
+		#A temporary array for the data
+		@data_arr = []
+		
+		#A temporary array for the headers, and a variable to track state
+		@head_arr = []
+		@gathering_state = 0
+		
 	
 		#For each line in the IO stream
 		self.each_line do |line|
@@ -65,56 +69,10 @@ class LogSeriesFile < File
 				# If it matches,
 				if m = line.match(lr)
 					
-					#Find out if it's a set, subsection, or none
-					case lr.type
-					
-					#When it's a set delimiter, 
-					when MatchType::SET
-					
-						#If it has a replacement string
-						if !lr.replacement_string.empty?
-							#send the temporary array to the csv
-							parseout << temparr
-						
-							#reset the temporary array to the replacement str
-							temparr = [m[lr.replacement_string.to_sym]]
+					# Call the decision engine
+					self.decision_engine(m,lr)
 
-						else
-							
-							#send the temporary array to the csv
-							parseout << temparr
-						
-							#reset the temporary array
-							temparr = []							
-						end
-					
-					#When it's a SUBSECTION delimiter, 
-					when MatchType::SUBSECTION
-					
-						#If it has a replacement string
-						if !lr.replacement_string.empty?
-						
-							#add a spacer and the replacement string to temparr
-							temparr.concat(["",m[lr.replacement_string.to_sym]])
-						else
-							
-							#add a spacer
-							temparr << ""
-							
-						end
-					
-					#When it's just a match, 
-					when MatchType::NONE
-						
-						#Add the contents of the match to the temporary array
-						temparr.concat(m.captures)
-					
-					#When the type is not set, or is none of the above
-					else
-						puts "LogRegexp has an invalid type"
-					end
-
-				end
+				end # If it matches...
 				
 				#no constraint on not matching
 				
@@ -124,14 +82,68 @@ class LogSeriesFile < File
 		end #For each line in the IO stream
 		
 		#Send any remaining temp array to the csv
-		parseout << temparr
+		@outfile << @data_arr
 		
 		#Then we close out the new csv
-		parseout.close
+		@outfile.close
 		
 	end
 	
+	
 	protected
+	
+	def decision_engine(m,lr)
+	
+		#Find out if it's a set, subsection, or none
+		case lr.type
+		
+		#When it's a set delimiter, 
+		when MatchType::SET
+		
+			#If it has a replacement string
+			if !lr.replacement_string.empty?
+				#send the temporary array to the csv
+				@outfile << @data_arr
+			
+				#reset the temporary array to the replacement str
+				@data_arr = [m[lr.replacement_string.to_sym]]
+
+			else
+				
+				#send the temporary array to the csv
+				@outfile << @data_arr
+			
+				#reset the temporary array
+				@data_arr = []							
+			end
+		
+		#When it's a SUBSECTION delimiter, 
+		when MatchType::SUBSECTION
+		
+			#If it has a replacement string
+			if !lr.replacement_string.empty?
+			
+				#add a spacer and the replacement string to @data_arr
+				@data_arr.concat(["",m[lr.replacement_string.to_sym]])
+			else
+				
+				#add a spacer
+				@data_arr << ""
+				
+			end
+		
+		#When it's just a match, 
+		when MatchType::NONE
+			
+			#Add the contents of the match to the temporary array
+			@data_arr.concat(m.captures)
+		
+		#When the type is not set, or is none of the above
+		else
+			puts "LogRegexp has an invalid type"
+		end
+	
+	end
 	
 	def debugout(thing)
 	
